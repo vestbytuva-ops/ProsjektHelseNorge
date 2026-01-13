d50811f84b555609e0301f04bef8bb82ff41ded2
vestbytuva-ops <vestby.tuva@gmail.com>
2026-01-13 11:58:04 +0100

    Endringer av MCQ, har blitt ferdig med det visuelle

---
+++ multiplechoice.js
@@ -0,0 +1,37 @@
+const multiplechoice = {
+    question : "Hvor mange timer sover du hver kveld?",
+    choice: [Mindre enn 4 timer, 4-6 timer, 6-8 timer, Mer enn 8 timer],
+    correctAnswer: "Mer enn 8 timer",
+    points: 4
+};
+const multiplechoice2 = {
+
+   question: "Hvor mange gram proteiner spiser du daglig?",
+   choice: ["Mindre enn 50 gram", "50-80 gram", "80-100 gram", "Mer enn 100 gram"],
+   correctAnswer: "Mer enn 100 gram",
+   points: 4
+};
+
+const multiplechoice3= {
+    question: "Hvor mange glass vann drikker du daglig?",
+    choice: ["Mindre enn 4 glass", "4-6 glass", "6-8 glass", "Mer enn 8 glass"],
+    correctAnswer: "Mer enn 8 glass",
+    points: 4
+};
+
+const multiplechoice4 = {
+    question: "Hvor mange minutter trener du ukentlig?",
+    choice: ["Mindre enn 60 minutter", "60-120 minutter", "120-180 minutter", "Mer enn 180 minutter"],
+    correctAnswer: "Mer enn 180 minutter",
+    points: 4
+};
+
+const multiplechoice5 = {
+    question: "Hvor mange porsjoner frukt og grønnsaker spiser du daglig?",
+    choice: ["Mindre enn 2 porsjoner", "2-3 porsjoner", "3-5 porsjoner", "Mer enn 5 porsjoner"],
+    correctAnswer: "Mer enn 5 porsjoner",
+    points: 4
+};
+
+
+export default multiplechoice;
